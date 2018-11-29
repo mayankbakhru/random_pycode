527e33bbae284f47942cb0a9f2dccbc0c6c513ab
Mayank <37677143+mayankbakhru@users.noreply.github.com>
2018-11-29 12:00:38 -0500

    Palindrome
    fun exercise to find if a string is a palindrome

---
+++ palindrome.py
@@ -0,0 +1,25 @@
+
+def main():
+    print("\ninput a string:")
+    str1=input()
+    print ("String", str1, "is a palindrome:",PD(str1.lower()))
+    # print(str1)
+
+def PD(str1):
+    strdict = {sd:0 for sd in str1}
+    result = False
+    odd_counter = 0
+    for item in str1:
+        strdict[item] = strdict[item] + 1
+    for key in strdict:
+        if (strdict[key] % 2 != 0):
+            odd_counter = odd_counter + 1
+    if (len(str1) % 2 == 0 and odd_counter == 0):
+        result = True
+    elif (len(str1) % 2 != 0 and odd_counter == 1):
+        result = True
+    else:
+        result = False
+    return result
+
+if __name__ == '__main__': main()
