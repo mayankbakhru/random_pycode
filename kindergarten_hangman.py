903f2f9216537d6cf25accdfc40e92e37776b5d4
Mayank <37677143+mayankbakhru@users.noreply.github.com>
2020-08-12 09:47:02 -0400

    Update kindergarten_hangman.py

--- kindergarten_hangman.py
+++ kindergarten_hangman.py
@@ -42,8 +42,20 @@ def guess(chosen,current, wrong_count,wrong_guess):
         res_flag = 0
     return (current,res_flag,wrong_count,wrong_guess)
 
+def start():
+    game()
+    while(True):
+        print ("Do you want to play again?(y/n):", end='')
+        x = input()
+        if (x.lower()!='y' and x.lower()!='n'):
+            while (x.lower()!='y' and x.lower()!='n'):
+                print("Invalid input! try again:",end='')
+                x = input()
+        if (x.lower()=='n'):
+            break
+        game()
 
-def main1():
+def game():
     # words = ['cute','girl','letter','father','mother','grandfather','family','grandmother']
     words = ['all','am','are','at','ate','be','black','brown','but','came','did','do','eat','four','get','good','have','he','into','like','must','new','no','now','on','our','out','please','pretty','ran','ride','saw','say','she','so','soon','that','there','they','this','too','under','want','was','well','went','what','white','who','will','with','yes']
     choose = random.randint(0,len(words)-1)
@@ -87,4 +99,4 @@ def print_hangman(pos):
             print(hangman[i])
  
 if __name__ == "__main__":
-    main1()
+    start()
